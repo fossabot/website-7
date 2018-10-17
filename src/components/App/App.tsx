fa81a66097c4437c5755b5b08d3610779e0ec0f8
Christoph Werner <codepunkt@users.noreply.github.com>
2018-10-17 09:14:08 +0200

    feat: automatically batch graphql requests
    * feat: automatically batch graphql requests
    * chore: add prettier configuration

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,37 @@
 import { Router } from '@reach/router'
-import ApolloClient from 'apollo-boost'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client'
+import { ApolloLink } from 'apollo-link'
+import { BatchHttpLink } from 'apollo-link-batch-http'
+import { onError } from 'apollo-link-error'
 import React, { Component } from 'react'
 import { ApolloProvider } from 'react-apollo'
 import HomePage from '../HomePage/HomePage'
 import WipPage from '../WipPage/WipPage'
 
 const client = new ApolloClient({
-  uri:
-    process.env.NODE_ENV === 'production'
-      ? 'https://api.paderbornjs.org'
-      : 'http://localhost:4000',
+  cache: new InMemoryCache(),
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        )
+      }
+      if (networkError) {
+        console.log(`[Network error]: ${networkError}`)
+      }
+    }),
+    new BatchHttpLink({
+      credentials: 'same-origin',
+      uri:
+        process.env.NODE_ENV === 'production'
+          ? 'https://api.paderbornjs.org'
+          : 'http://localhost:4000',
+    }),
+  ]),
 })
 
 class App extends Component {
