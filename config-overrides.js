6e1e0e6709a1395f8937e3f42f103d1fffe8f8f9
Christoph Werner <codepunkt@users.noreply.github.com>
2018-10-17 10:50:40 +0200

    build: add bundle analyzer (#11)

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,19 @@
+const { rewireEmotion } = require('react-app-rewire-emotion')
 const {
   rewireWebpack: rewireTypescript,
   rewireJest: rewireTypescriptJest,
   rewireTSLint,
 } = require('react-app-rewire-typescript-babel-preset')
-const { rewireEmotion } = require('react-app-rewire-emotion')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
 
 module.exports = {
   webpack: function(config, env) {
+    const ifEnv = check => (ifBranch, elseBranch = null) =>
+      env === check ? ifBranch : elseBranch
+    const ifDev = ifEnv('development')
+    const ifProd = ifEnv('production')
+
     config = rewireTypescript(config)
     config = rewireTSLint(config /* {} - optional tslint-loader options */)
     if (env === 'production' || env === 'test') {
@@ -14,8 +21,23 @@ module.exports = {
     } else {
       config = rewireEmotion(config, env, { autoLabel: true, sourceMap: true })
     }
-    return config
+
+    return {
+      ...config,
+      ...ifDev({ devtool: 'source-map' }),
+      plugins: [
+        ...config.plugins,
+        ...ifProd([
+          new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            openAnalyzer: false,
+            reportFilename: 'bundle-report.html',
+          }),
+        ]),
+      ],
+    }
   },
+
   // When using jest, the code is not compiled via webpack. Any changes made to
   // webpack's babel config above (here: rewiring babel-plugin-emotion) will
   // not be applied when running tests in jest.
