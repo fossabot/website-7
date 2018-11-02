23dae2980681a4d6151f4fc0856dfc9a76019244
Christoph Werner <codepunkt@users.noreply.github.com>
2018-11-02 10:44:39 +0100

    build: use create-react-app typescript support (#17)
    * build: use create-react-app typescript support
    * fix: add .js files to tslint ignore

--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,9 @@
 const { rewireEmotion } = require('react-app-rewire-emotion')
-const {
-  rewireWebpack: rewireTypescript,
-  rewireJest: rewireTypescriptJest,
-  rewireTSLint,
-} = require('react-app-rewire-typescript-babel-preset')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin
 
 module.exports = {
   webpack: function(config, env) {
-    const ifEnv = check => (ifBranch, elseBranch = null) =>
-      env === check ? ifBranch : elseBranch
-    const ifDev = ifEnv('development')
-    const ifProd = ifEnv('production')
-
-    config = rewireTypescript(config)
-    config = rewireTSLint(config /* {} - optional tslint-loader options */)
     if (env === 'production' || env === 'test') {
       config = rewireEmotion(config, env, { hoist: true })
     } else {
@@ -24,26 +12,19 @@ module.exports = {
 
     return {
       ...config,
-      ...ifDev({ devtool: 'source-map' }),
+      ...(env === 'development' ? { devtool: 'source-map' } : {}),
       plugins: [
         ...config.plugins,
-        ...ifProd([
-          new BundleAnalyzerPlugin({
-            analyzerMode: 'static',
-            openAnalyzer: false,
-            reportFilename: 'bundle-report.html',
-          }),
-        ]),
+        ...(env === 'production'
+          ? [
+              new BundleAnalyzerPlugin({
+                analyzerMode: 'static',
+                openAnalyzer: false,
+                reportFilename: 'bundle-report.html',
+              }),
+            ]
+          : []),
       ],
     }
-  },
-
-  // When using jest, the code is not compiled via webpack. Any changes made to
-  // webpack's babel config above (here: rewiring babel-plugin-emotion) will
-  // not be applied when running tests in jest.
-  // In order to load babel plugins/presets during testing, an external .babelrc
-  // configuration file is set up, targetting the "test" environment.
-  jest: function(config) {
-    return rewireTypescriptJest(config)
   },
 }
